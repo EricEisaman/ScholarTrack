159a522b53c76cae51b7a4810597afccbcdb0859
Eric Eisaman <eric@framevr.io>
2025-08-24 10:50:18 -0400

    Fix build-static.js to use ES modules syntax - Convert require() to import statements - Add __dirname equivalent for ES modules - Fixes Docker build error with client package.json type: module

--- client/build-static.js
+++ client/build-static.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create a simple static build for Docker
 const htmlContent = `<!DOCTYPE html>
